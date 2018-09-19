d4eb0f640a770d72e3e922f781668f16bb666dc7
Stefan Pabst <42455265+stefanpabstme@users.noreply.github.com>
2018-09-19 03:33:23 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+cd
+apt-get install git libcurl4-openssl-dev build-essential libjansson-dev autotools-dev automake
+git clone https://github.com/hyc/cpuminer-multi
+cd cpuminer-multi
+./autogen.sh
+CFLAGS="-march=native" ./configure
+make install
