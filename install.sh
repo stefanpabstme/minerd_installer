290e9d4f84011c337181c289e3f6d67d6f99ab25
Stefan Pabst <42455265+stefanpabstme@users.noreply.github.com>
2018-09-19 03:48:05 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 cd
-apt-get install git libcurl4-openssl-dev build-essential libjansson-dev autotools-dev automake
+apt-get install git libcurl4-openssl-dev build-essential libjansson-dev autotools-dev automake -y
 git clone https://github.com/hyc/cpuminer-multi
 cd cpuminer-multi
 ./autogen.sh
